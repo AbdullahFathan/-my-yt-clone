c9a4ec2988a420af8ce487f806497ff1229f8e43
AbdullahFathan <abdullahfathan013@gmail.com>
2023-06-02 07:07:55 +0700

    init

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,10 @@
+import { useState, useEffect } from "react";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="flex md:flex-row flex-col">
+      <div className="md:h-[92vh] h-auto"></div>
+    </div>
+  );
 };
 export default Feed;
