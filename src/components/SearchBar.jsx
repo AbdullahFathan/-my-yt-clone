37822d933a7a22611088973bb5c4b57bac9d20ad
AbdullahFathan <abdullahfathan013@gmail.com>
2023-06-09 17:15:43 +0700

    feat: search video done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,20 +3,37 @@ import { useNavigate } from "react-router-dom";
 import { SearchIcon } from "../assets";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm(" ");
+    }
+  };
+
   return (
-    <div className="bg-white rounded-[20px]  border-solid border-2 border-[#E3E3E3] pl-2 sm:mr-5 ">
-      <form className="flex justify-center items-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="search-bar"
-          onChange={() => {}}
-        />
-        <button type="submit" className="p-[10px]">
-          <img src={SearchIcon} />
-        </button>
-      </form>
-    </div>
+    <form
+      className="bg-white rounded-[20px]  border-solid border-2 border-[#E3E3E3] pl-2 sm:mr-5 flex justify-center items-center"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-bar"
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <button type="submit" className="p-[10px]">
+        <img src={SearchIcon} />
+      </button>
+    </form>
   );
 };
 export default SearchBar;
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+import { Videos } from "./";
+import { getVideoApi } from "../services/getVideo";
+import { useParams } from "react-router-dom";
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    getVideoApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <div className="flex-[2]  overflow-y-auto m-5 ">
+      <h4 className="text-white font-bold mb-4  text-4xl">
+        Search For: <span className="text-[#FC1503]">{searchTerm}</span> Videos
+      </h4>
+      <Videos videos={videos} />
+    </div>
+  );
 };
 export default SearchFeed;
